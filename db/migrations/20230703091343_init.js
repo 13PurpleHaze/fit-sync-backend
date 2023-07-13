a3b6edb54535e252247f14eefd737d635ed4ac9b
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2023-07-13 16:54:31 +0500

    fix: correct migration file

--- db/migrations/20230703091343_init.js
+++ db/migrations/20230703091343_init.js
@@ -11,21 +11,21 @@ export const up = function(knex) {
             table.string('img', 255);
             table.timestamps(true, true);
         })
+        .createTable('roles', function (table) {
+            table.bigIncrements('role_id');
+            table.string('title').notNullable();
+        })
         .createTable('users', function (table) {
             table.bigIncrements('user_id');
             table.string('first_name').notNullable();
             table.string('sur_name').notNullable();
             table.integer('age').notNullable();
             table.boolean('gender').defaultTo(true);
-            table.boolean('is_admin').defaultTo(false);
-            table.boolean('is_block').defaultTo(false);
-            table.timestamps(true, true);
-        })
-        .createTable('user_details', function (table) {
-            table.bigInteger('user_id').references('user_id').inTable('users').notNullable();
-            table.primary('user_id');
             table.string('login').notNullable();
             table.string('password').notNullable();
+            table.bigInteger('role_id').defaultTo(1).references('role_id').inTable('roles');
+            table.boolean('status').defaultTo(true);
+            table.timestamps(true, true);
         })
         .createTable('workouts', function (table) {
             table.bigIncrements('workout_id');
@@ -36,6 +36,7 @@ export const up = function(knex) {
         .createTable('workout_exercises', function (table) {
             table.bigInteger('workout_id').references('workout_id').inTable('workouts').notNullable();
             table.bigInteger('exercise_id').references('exercise_id').inTable('exercises').notNullable();
+            table.integer('reps').notNullable();
             table.primary(['workout_id', 'exercise_id']);
         })
         .createTable('sessions', function (table) {
@@ -53,6 +54,14 @@ export const up = function(knex) {
             table.bigInteger('user_id').references('user_id').inTable('users').notNullable();
             table.text('text');
             table.timestamps(true, true);
+        })
+        .createTable('user_exercises', function (table) {
+            table.bigIncrements('user_exercise_id');
+            table.bigInteger('session_id').references('session_id').inTable('sessions').notNullable();
+            table.bigInteger('user_id').references('user_id').inTable('users').notNullable();
+            table.bigInteger('exercise_id').references('exercise_id').inTable('exercises').notNullable();
+            table.integer('reps').notNullable();
+            table.timestamps(true, true);
         });
 };
 
@@ -62,12 +71,13 @@ export const up = function(knex) {
  */
 export const down = function(knex) {
     return knex.schema
+        .dropTable('user_exercises')
         .dropTable('messages')
         .dropTable('session_users')
         .dropTable('sessions')
         .dropTable('workout_exercises')
         .dropTable('workouts')
-        .dropTable('user_details')
         .dropTable('users')
+        .dropTable('roles')
         .dropTable('exercises');
 };
